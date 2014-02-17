dcc66f46623ee1a67a6ef1cccc181f643de076a4
jeremie <j.bellec@structure-computation.com>
2014-02-17 18:18:56 +0100

    version sc (2)

--- GlobalManagerPlugin/ServerPlugin/src/main.cpp
+++ GlobalManagerPlugin/ServerPlugin/src/main.cpp
@@ -12,9 +12,10 @@ int main( int argc, char **argv ) {
     // type d'item observé
     sc.reg_type( "TestFlo1Item" ); 
     sc.reg_type( "TestRenoItem" );  
-    sc.reg_type( "File" );
-    sc.reg_type( "Img" );
-    sc.reg_type( "ServerAssistedVisualization" );
+    sc.reg_type( "CorrelationItem" );
+    //sc.reg_type( "File" );
+    //sc.reg_type( "Img" );
+    //sc.reg_type( "ServerAssistedVisualization" );
     
     //liste des job en cours
     JobList job_list;
