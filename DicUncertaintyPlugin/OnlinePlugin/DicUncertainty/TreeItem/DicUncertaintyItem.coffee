ea1153189b1cc2d2aa82423428cd263f7732f4db
renaud <renaud@toto.(none)>
2014-05-22 14:42:21 +0200

    premiere version DICUncertainty

--- DicUncertaintyPlugin/OnlinePlugin/DicUncertainty/TreeItem/DicUncertaintyItem.coffee
+++ DicUncertaintyPlugin/OnlinePlugin/DicUncertainty/TreeItem/DicUncertaintyItem.coffee
@@ -8,23 +8,38 @@ class DicUncertaintyItem extends TreeItem_Computable
         @_ico.set "img/mesher.png"
         @_viewable.set false        
         @add_child new ImgSetItem
-        @add_child new MaskItem
+        @add_child new SketchItem
         @add_child new DiscretizationItem
         
         @add_output new FieldSetCorreliItem
         
         @add_attr
-           noise_level : 1
            add_noise : false
-        
+           noise_level : 1
+           uncertainty_type : new Choice( 0, [ "Displacement", "Strain"] )
+           specific_mesh : false
+           min_value : 0
+           max_value : 1
+           step : 0.5
+           _graph : new Graph 
+                marker: 'dot',
+                marker_color: "#f00"
+                line_width  : 3,
+                line_color: "#f00"
+                marker_size: 10,
+                font_size : 24,
+                legend_x_division: 10,
+                legend_y_division: 10,
+                x_axis: 'D (pix)',
+                y_axis: 'Uncertainty'
 #    display_suppl_context_actions: ( context_action )  ->
         
     accept_child: ( ch ) ->
         ch instanceof MaskItem or 
         ch instanceof DiscretizationItem or
         ch instanceof SketchItem or 
-        ch instanceof ImgSetItem or
-        ch instanceof TransformItem
+        ch instanceof ImgSetItem
+#         ch instanceof TransformItem
         
 #    sub_canvas_items: ->
 #        [ @_mesh ]
--- DicUncertaintyPlugin/ServerPlugin/src/DicUncertaintyUpdater.cpp
+++ DicUncertaintyPlugin/ServerPlugin/src/DicUncertaintyUpdater.cpp
@@ -3,13 +3,220 @@
 #include <QtCore/QFile>
 #include <QtCore/QTemporaryFile>
 #include <QtCore/QDataStream>
+#include <QtCore/QByteArray>
+#include <QtCore/QBuffer>
+#include <QtGui/QImage>
 #include "DicUncertaintyUpdater.h"
-
+#include <mesh/make_rect.h>
+#include <mesh/make_mesh_from_mask.h>
+#include <correlation/DicCPU.h>
+#include <sstream>
+#include <string.h>
+#include "dec_image.h"
+#include "make_fields.h"
 
 bool DicUncertaintyUpdater::run( MP mp ) {
     
-    MP ch = mp[ "_children[ 0 ]" ];
+    MP ch = mp[ "_children" ];
+    const unsigned int dim = 2;
+
+    typedef DicCPU<double,dim>::TM_exemple TM;
+    typedef TM::Pvec Pvec;
+    typedef ImgInterp<double,dim/*,ImgInterpOrder3_Kernel*/> I2;
+    typedef LMT::DicCPU<double,dim/*,false*/> DC;
     
+    MP mesh;
+    
+    TM dic_mesh;
+    LMT::Vec<I2> images;
+    Vec<int,dim> xmin (1e6,1e6);
+    Vec<int,dim> xmax (-1e6,-1e6);
+    // output
+    MP output_field = mp[ "_output[ 0 ]" ];
+    
+    QVector<MP> displacements = make_field( output_field, dic_mesh.dim, "Displacement" );
+    
+    for( int i = 0; i < ch.size(); ++i ) {
+        MP c = ch[ i ];
+        qDebug() <<  c.type() ;
+        // Load reference image
+        if ( c.type() == "ImgSetItem" ) {
+            for( int j = 0; j < c[ "_children" ].size(); ++j ) {
+                MP im = c[ "_children" ][ j ];
+                if ( im.type() == "ImgItem" ) {
+                    I2 *img = images.new_elem();
+                    QString name = im[ "img.src" ];
+//                     if ( im[ "img.src" ].type() != "Path" )
+//                         name = "../CorreliOnline/html/" + name;
+
+                    try {
+                        img->load( name.toAscii().data() );
+                        img->reverse_y();
+                    } catch ( std::string msg ) {
+                        add_message( mp, Updater::ET_Error, "Img " + name + " does not exist" );
+                        return false;
+                    }
+                } /*else if ( im.type() == "RawVolume" ) {
+                    I2 *img = images.new_elem();
+                    MP ms( im[ "img_size" ] );
+                    Vec<int,3> S( ms[ 0 ], ms[ 1 ], ms[ 2 ] );
+                    PRINT( S );
+
+                    MP volume = im[ "_children" ][ 0 ];
+                    qDebug() << volume;
+                    MP data = updater->sc->load_ptr( volume[ "_ptr" ] );
+                    qDebug() << data;
+
+                    QString name = data;
+                    PRINT( name.toAscii().data() );
+
+                    try {
+                        typedef unsigned char PI8;
+                        img->template load_binary<PI8>( name.toAscii().data(), S );
+                    } catch ( std::string msg ) {
+                        updater->add_message( mp, Updater::ET_Error, "Img " + name + " does not exist" );
+                        return false;
+                    }
+                    PRINT( name.toAscii().data() );
+                }*/
+            }
+        }
+        // Mesh generation
+        // load existing mesh
+        if ( int(mp[ "specific_mesh" ]) ){
+            if ( c.type() == "DiscretizationItem" ) {
+                mesh.reassign( c[ "_children._mesh" ] );
+                for( int i = 0, n = mesh[ "points" ].size(); i < n; ++i ) {
+                    MP pos = mesh[ "points" ][ i ][ "pos" ];
+                    dic_mesh.add_node( Pvec( pos[ 0 ], pos[ 1 ], pos[ 2 ] ) );
+                }
+
+                for( int nel = 0, mel = mesh[ "_elements" ].size(); nel < mel; ++nel ) {
+                    MP el = mesh[ "_elements" ][ nel ];
+                    if ( el.type() == "Element_TriangleList" and dim == 2 ) {
+                        if ( TypedArray<int> *indices = dynamic_cast<TypedArray<int> *>( el[ "indices" ].model() ) ) {
+                            for( int nt = 0, ct = 0; nt < indices->size( 1 ); ++nt ) {
+                                unsigned o[ 3 ];
+                                for( int j = 0; j < 3; ++j )
+                                    o[ j ] = qMin( indices->operator[]( ct++ ), (int)dic_mesh.node_list.size() - 1 );
+                                dic_mesh.add_element( Triangle(), DefaultBehavior(), o );
+                            }
+                        }
+                    } else if ( el.type() == "Element_TetrahedraList" and dim == 3 ) {
+                        if ( TypedArray<int> *indices = dynamic_cast<TypedArray<int> *>( el[ "indices" ].model() ) ) {
+                            for( int nt = 0, ct = 0; nt < indices->size( 1 ); ++nt ) {
+                                unsigned o[ 4 ];
+                                for( int j = 0; j < 4; ++j )
+                                    o[ j ] = qMin( indices->operator[]( ct++ ), (int)dic_mesh.node_list.size() - 1 );
+                                dic_mesh.add_element( Tetra(), DefaultBehavior(), o );
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // Mesh generation from sketch
+        if ( c.type() == "SketchItem" ) {
+            mesh.reassign( c[ "mesh" ] );
+            PRINT(mesh[ "points" ].size());
+            for( int i = 0, n = mesh[ "points" ].size(); i < n; ++i ) {
+                MP pos = mesh[ "points" ][ i ][ "pos" ];
+                xmin = min( xmin , Pvec (pos[0],pos[1]) );
+                xmax = max( xmax , Pvec (pos[0],pos[1]) );
+            }
+            PRINT(xmin);
+            PRINT(xmax);
+            Pvec XD(20,20);
+            make_rect( dic_mesh, Quad(), xmin-xmin, xmax-xmin, XD );
+        }
+    }
+
+    // Load parameters
+    bool add_noise = false;
+    if ( int (mp[ "add_noise" ] ) )
+        add_noise = true;
+    double noise_level = mp[ "noise_level" ];
+    int uncertainty_type = mp[ "uncertainty_type.num" ];
+    double min_value = mp[ "min_value" ];
+    double max_value = mp[ "max_value" ];
+    double step = mp[ "step" ];
+   
+    // Deform reference image
+    Vec<double> Dimp;
+    if ( uncertainty_type == 0 ){ // displacement uncertainty
+        // Generate shifted images
+        PRINT("displacement uncertainty");
+        for (double i=min_value;i<=max_value;i+=step){
+            I2 *img = images.new_elem();
+            *img = dec_image(images[0], xmin, xmax, i, i, add_noise, noise_level );
+            Dimp.push_back(i);
+        }
+        for (int i=0;i<images.size();++i) images[i].save( "toto"+to_string(i)+".png" );
+    }
+    else if ( uncertainty_type == 1 ){// strain uncertainty
+        // Generate strain vector
+        PRINT("strain uncertainty");
+        // Genrate strained images
+    }
+
+    Vec<double> UncX; UncX.resize(images.size() - 1);
+    Vec<double> ErrX; ErrX.resize(images.size() - 1);
+    UncX[0] = 0;
+    ErrX[0] = 0;
+    Vec<double> UncY; UncY.resize(images.size() - 1);
+    Vec<double> ErrY; ErrY.resize(images.size() - 1);
+    UncY[0] = 0;
+    ErrY[0] = 0;
+    for( int j = 2; j < images.size(); ++j ) {
+        // DIC on series of images
+        DC dic;
+        dic.min_norm_inf_dU = 1e-3;
+        dic.div_pixel = 1;
+        dic.max_cpt_iter = 10;
+        dic.multi_resolution = 0;
+ //     calcul du champ de deplacement obtenu par correlation
+        dic.exec( images[1],images[j] , dic_mesh , dep_DM(), lum_DM() );
+        // disp field
+        for( int d = 0; d < dic_mesh.dim; ++d ) {
+            // data
+            QVector<int> s; s << dic_mesh.node_list.size();
+            TypedArray<double> *data = new TypedArray<double>( s );
+            Vec<double> Ux; Ux.resize(dic_mesh.node_list.size());
+            Vec<double> Uy; Uy.resize(dic_mesh.node_list.size());
+            for( int i = 0; i < dic_mesh.node_list.size(); ++i ){
+                data->operator[]( i ) = dic_mesh.node_list[ i ].dep[ d ];
+                if (d==0) 
+                 Ux[i] = dic_mesh.node_list[ i ].dep[ d ] ;
+                if (d==1) 
+                 Uy[i] = dic_mesh.node_list[ i ].dep[ d ] ;
+                dic_mesh.node_list[ i ].dep[ d ] = 0.0;
+            }
+            
+            // NodalField
+            add_field_in_Interpolated( displacements[ d ], mesh, data, j );
+            if (d==0){
+                UncX[j-1] = standard_deviation(Ux);
+                ErrX[j-1] = mean(Ux) - Dimp[j-1];
+//                 PRINT(Dimp[j-1]);
+//                 PRINT(mean(Ux));
+            }
+            if (d==1){
+                UncY[j-1] = standard_deviation(Uy);
+                ErrY[j-1] = mean(Uy) - Dimp[j-1];
+//                 PRINT(Dimp[j-1]);
+//                 PRINT(mean(Uy));
+            }
+        }
+    }
+    // Calculate mean and standard deviation for the series of field (strain or displacement)
+    PRINT(UncX);
+    PRINT(ErrX);
+    PRINT(UncY);
+    PRINT(ErrY);
+    // Calculate difference between results and imposed displacement or strain
+
+    // Plot graphs
+
 }
 
 
