34bbe3d06b51c86a22b86071059653963bc80b56
EikoSimComputer1 <eikosimcomputer1@eikosimcomputer1-Precision-Tower-5810.(none)>
2015-05-28 13:37:12 +0200

    modif Dicuncertainty format sortie en treeItem

--- DicUncertaintyPlugin/ServerPlugin/src/DicUncertaintyUpdater.h
+++ DicUncertaintyPlugin/ServerPlugin/src/DicUncertaintyUpdater.h
@@ -1,7 +1,17 @@
 #ifndef DICUNCERTAINTYUPDATER_H
 #define DICUNCERTAINTYUPDATER_H
-
+#include <Soca/Com/SodaClient.h>
 #include <Soca/Updater.h>
+// #include <containers/vec.h>
+// #include "../../../../Soda/src/Soda/Sys/Vec.h"
+
+// #include "../../../../LMTpp/include/containers/vec.h"
+//using namespace LMT;
+// #include "LMT/include/containers/mat.h"
+#include <containers/mat.h>
+//  puis utiliser l'espace LMT
+    using namespace std;
+    using namespace LMT;
 class SodaClient;
 class QDataStream;
 
@@ -11,9 +21,16 @@ class DicUncertaintyUpdater : public Updater {
 protected:
     bool run( MP mp );
     virtual QString type() const { return "DicUncertaintyUpdater"; }
+    void set_vecToTreeItemVec(const Vec<double> &vec, MP MPvec, MP mpTreeItem_Vec, QString attrName);
     
 public:
     SodaClient *sc;
+    void new_lst(const char* arg1);
+    void print(const char* arg1);
+    //void qDebug(const char* arg1);
+  //  void PRINT(Vec< double > ErrY);
+//     void mean(Vec< double > Ux);
+//double mean(Vec< double > Ux);//TEST
 };
 
 #endif // DICUNCERTAINTYUPDATER_H
